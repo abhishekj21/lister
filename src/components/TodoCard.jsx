6d9bbc9f4854dafaa9168eef7188aea6dac269bf
Abhishek Singh <singhabhishekj21@gmail.com>
2025-01-10 05:52:21 +0000

    task card feature

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,3 +1,18 @@
-export function TodoCard() {
-  return <div></div>;
+export function TodoCard(props) {
+  const { todoIndex, todos } = props;
+  const todo = todos[todoIndex];
+  console.log(todo);
+  return (
+    <div className="card todo-item">
+      <p>{todo.input}</p>
+      <div className="todo-button">
+        <button>
+          <h6>Done</h6>
+        </button>
+        <button>
+          <h6>Delete</h6>
+        </button>
+      </div>
+    </div>
+  );
 }
